d7056ed3d88074a0f5fc108c186040fe0cbd9fe2
gabrielgadottidecampos <gabriel-gadotti@hotmail.com>
2023-11-26 00:38:40 -0300

    feat:criado migration Cliente

--- app_academia/database/migrations/2023_11_26_032102_create_clientes_table.php
+++ app_academia/database/migrations/2023_11_26_032102_create_clientes_table.php
@@ -13,13 +13,22 @@ return new class extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 30);
+            $table->dateTime('data_nascimento');
+            $table->char('sexo', 1);
+            $table->float('altura', 3, 2);
+            $table->float('peso', 5, 2);
+            $table->string('cidade');
+            $table->string('endereco');
+            $table->string('rua');
+            $table->string('numero', 30);
+            $table->string('complemento');
+            $table->string('telefone1', 14);
+            $table->string('telefone2', 14);
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('clientes');
